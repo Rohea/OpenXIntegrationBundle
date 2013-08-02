68963a560d7d812777e41b205677a42b309e8274
TomiS <tomi.saarinen@rohea.com>
2013-08-02 16:05:41 +0300

    Config stuff

--- Service/ApiClientFactory.php
+++ Service/ApiClientFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace Rohea\OpenXClientBundle\Service;
+namespace Rohea\OpenXIntegrationBundle\Service;
 
 use \LogicException;
 use \InvalidArgumentException;
@@ -10,13 +10,31 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApiClientFactory {
 
-    public function __construct()
-    {
+    /**
+     * $var array OpenX server configurations from config
+     */
+    private $servers;
 
+    public function __construct($servers)
+    {
+        $this->servers = $servers;
     }
 
-    public function getClient($context)
+    /**
+     * Get instance of OpenX XML-RPC Api client
+     *
+     * @param string $server key that matches configuration
+     */
+    public function getClient($server)
     {
+        if (! isset($this->servers[$server])) {
+            throw new \InvalidArgumentException("Server '$server' is not defined in configuration.");
+        }
+
+        $config = $this->servers[$server];
+
+var_dump($config);
+die();
 
     }
 
